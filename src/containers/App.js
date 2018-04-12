6cbd99b8590a8ad6a6b5bda0f89cfbc9cfefbbc0
Leonardo Schlossmacher <e@leods92.com>
2018-04-13 00:37:50 +0200

    Add mock data and its loading

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.fetchEntity()
+      .then(() => this.forceUpdate());
+  }
+
+  fetchEntity() {
+    return fetch('api/company.json')
+      .then(response => response.json())
+      .then(entity => this.entity = entity)
+    ;
+  }
+
   render() {
     return (
       <div className="App">
