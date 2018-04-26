b6e512e5f6af4920788ab9da55eca34b3d2cf498
Leonardo Schlossmacher <e@leods92.com>
2018-04-26 20:30:55 +0200

    Move api mocks into a separate directory

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ export const API_MODE = {
 
 export const API = {
   basePath: {
-    [API_MODE.mocked]: 'api',
+    [API_MODE.mocked]: 'mocked_api',
     [API_MODE.live]: 'https://zgif.org/api'
   }
 };
